5cd49a86ba31f35c3d776e18aaa8c402d5a74eff
Frank Milne <frank@70kft.com>
2013-08-13 16:27:44 -0500

    fuzzbuzz

---
+++ FizzBuzz/app/controllers/compute_fizz_buzz_controller.rb
@@ -0,0 +1,23 @@
+class ComputeFizzBuzzController < ApplicationController
+  def default
+    @fizzbuzznumbers = []
+    (1..100).each {|number|
+      @fizzbuzznumbers << computeFizzBuzz(number)}
+  end
+   
+   def number
+    @fizzbuzznumber = computeFizzBuzz(params[:num].to_i)
+    respond_to do |format|
+      format.html
+      format.text { render :text => @fizzbuzznumber }
+    end
+  end
+   
+  private
+  def computeFizzBuzz(number)
+    return "FizzBuzz" if 0 == number % 15
+    return "Fizz" if 0 == number % 3
+    return "Buzz" if 0 == number % 5
+    number
+  end
+end
---
+++ FizzBuzz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FizzBuzz::Application.config.secret_token = '64e175d9d332f87bef1e75252fe244a14458b91b3c6fa36ad9c9d4c8904877555eb39155be2b462025bee50010a5e9b72d5e6a8a66f039ea341158eab696d08c'
